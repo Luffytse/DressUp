07bf44dab88770f342a5abd18f3d1c51fe408e7b
Lufei Xie <luffytse@me.com>
2023-02-04 00:20:03 -0800

    0. Initial Setup With Spring Boot

---
+++ src/main/java/com/luffy/dressup/DressupApplication.java
@@ -0,0 +1,13 @@
+package com.luffy.dressup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DressupApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DressupApplication.class, args);
+  }
+
+}
